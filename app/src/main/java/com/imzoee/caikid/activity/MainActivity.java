147d4da03d44a3b13e7746149aacbf0d4645c4e3
Zoey Chang <Zoey Chang>
2016-04-26 15:08:27 +0800

    backup

--- app/src/main/java/com/imzoee/caikid/activity/MainActivity.java
+++ app/src/main/java/com/imzoee/caikid/activity/MainActivity.java
@@ -1,8 +1,9 @@
- package com.imzoee.caikid.activity;
+package com.imzoee.caikid.activity;
 
 import java.util.ArrayList;
 
 import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
@@ -26,13 +27,13 @@ import android.support.v4.view.ViewPager.OnPageChangeListener;
 import com.imzoee.caikid.adapter.RecipeAdapter;
 import com.imzoee.caikid.R;
 
- public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
-     ViewPager pager = null;
-     PagerTabStrip tabStrip = null;
-     ArrayList<View> viewContainter = new ArrayList<View>();
-     ArrayList<String> titleContainer = new ArrayList<String>();
-     public String TAG = "tag";                        // debug used
+    ViewPager pager = null;
+    PagerTabStrip tabStrip = null;
+    ArrayList<View> viewContainter = new ArrayList<>();
+    ArrayList<String> titleContainer = new ArrayList<>();
+    public String TAG = "tag";                        // debug used
 
 
     @Override
@@ -44,42 +45,70 @@ import com.imzoee.caikid.R;
 
     }
 
-     /*
-      * init the data for this activity
-      */
-     private void initData(){
+    /*
+     * call only when restart the activity
+     */
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
 
-     }
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    /*
+             * init the data for this activity
+             */
+    private void initData() {
+
+    }
 
 
-     /*
-      * init the view for this activity
-      *
-      * include
-      * 1. viewpager
-      * 2. tab strip
-      * 3.
-      */
-     private void initView(){
-         setContentView(R.layout.activity_main);
+    /*
+     * init the view for this activity
+     *
+     * include
+     * 1. viewpager
+     * 2. tab strip
+     * 3.
+     */
+    private void initView() {
+        setContentView(R.layout.activity_main);
 
-         //ViewGroup root = (ViewGroup)this.findViewById(android.R.id.content);
+        //ViewGroup root = (ViewGroup)this.findViewById(android.R.id.content);
 
-         pager = (ViewPager) this.findViewById(R.id.viewpager);
-         tabStrip = (PagerTabStrip) this.findViewById(R.id.tabstrip);
+        pager = (ViewPager) this.findViewById(R.id.viewpager);
+        tabStrip = (PagerTabStrip) this.findViewById(R.id.tabstrip);
 
-         tabStrip.setDrawFullUnderline(false);//取消tab下面的长横线
-         tabStrip.setBackgroundColor(this.getResources().getColor(R.color.mainBg));//设置tab的背景色
-         tabStrip.setTabIndicatorColor(this.getResources().getColor(R.color.tabDownLine));//设置当前tab页签的下划线颜色
-         tabStrip.setTextSpacing(200);
+        tabStrip.setDrawFullUnderline(false);//取消tab下面的长横线
+        tabStrip.setBackgroundColor(this.getResources().getColor(R.color.mainBg));//设置tab的背景色
+        tabStrip.setTabIndicatorColor(this.getResources().getColor(R.color.tabDownLine));//设置当前tab页签的下划线颜色
+        tabStrip.setTextSpacing(3);
 
-         initViewPager();
-     }
+        initViewPager();
+    }
 
-     private void initViewPager(){
-         View view1 = LayoutInflater.from(this).inflate(R.layout.tab_main_recipe, null);
-         View view2 = LayoutInflater.from(this).inflate(R.layout.tab_main_order, null);
-         View view3 = LayoutInflater.from(this).inflate(R.layout.tab_main_me, null);
+    private void initViewPager() {
+        View view1 = LayoutInflater.from(this).inflate(R.layout.tab_main_recipe, null);
+        View view2 = LayoutInflater.from(this).inflate(R.layout.tab_main_order, null);
+        View view3 = LayoutInflater.from(this).inflate(R.layout.tab_main_me, null);
 
 
         /*
@@ -89,95 +118,95 @@ import com.imzoee.caikid.R;
         */
 
 
-         //viewpager开始添加view
-         viewContainter.add(view1);
-         viewContainter.add(view2);
-         viewContainter.add(view3);
-         //页签项
-         titleContainer.add(getString(R.string.tab_recipe));
-         titleContainer.add(getString(R.string.tab_order));
-         titleContainer.add(getString(R.string.tab_pay));
+        //viewpager开始添加view
+        viewContainter.add(view1);
+        viewContainter.add(view2);
+        viewContainter.add(view3);
+        //页签项
+        titleContainer.add(getString(R.string.tab_recipe));
+        titleContainer.add(getString(R.string.tab_order));
+        titleContainer.add(getString(R.string.tab_pay));
 
 
          /*----------------------------------- pager adapter ----------------------------------------------*/
-         pager.setAdapter(new PagerAdapter() {
+        pager.setAdapter(new PagerAdapter() {
 
-             //viewpager中的组件数量
-             @Override
-             public int getCount() {
-                 return viewContainter.size();
-             }
+            //viewpager中的组件数量
+            @Override
+            public int getCount() {
+                return viewContainter.size();
+            }
 
-             //滑动切换的时候销毁当前的组件
-             @Override
-             public void destroyItem(ViewGroup container, int position,
-                                     Object object) {
-                 ((ViewPager) container).removeView(viewContainter.get(position));
-             }
+            //滑动切换的时候销毁当前的组件
+            @Override
+            public void destroyItem(ViewGroup container, int position,
+                                    Object object) {
+                ((ViewPager) container).removeView(viewContainter.get(position));
+            }
 
-             //每次滑动的时候生成的组件
-             @Override
-             public Object instantiateItem(ViewGroup container, int position) {
-                 ((ViewPager) container).addView(viewContainter.get(position));
-                 return viewContainter.get(position);
-             }
+            //每次滑动的时候生成的组件
+            @Override
+            public Object instantiateItem(ViewGroup container, int position) {
+                ((ViewPager) container).addView(viewContainter.get(position));
+                return viewContainter.get(position);
+            }
 
-             @Override
-             public boolean isViewFromObject(View arg0, Object arg1) {
-                 return arg0 == arg1;
-             }
+            @Override
+            public boolean isViewFromObject(View arg0, Object arg1) {
+                return arg0 == arg1;
+            }
 
-             @Override
-             public int getItemPosition(Object object) {
-                 return super.getItemPosition(object);
-             }
+            @Override
+            public int getItemPosition(Object object) {
+                return super.getItemPosition(object);
+            }
 
-             @Override
-             public CharSequence getPageTitle(int position) {
-                 return titleContainer.get(position);
-             }
-         });
+            @Override
+            public CharSequence getPageTitle(int position) {
+                return titleContainer.get(position);
+            }
+        });
 
          /*---------------------------------- pager listener ----------------------------------------------*/
-         pager.setOnPageChangeListener(new OnPageChangeListener() {
-             @Override
-             public void onPageScrollStateChanged(int arg0) {
-                 Log.d(TAG, "--------changed:" + arg0);
-             }
+        pager.setOnPageChangeListener(new OnPageChangeListener() {
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+                Log.d(TAG, "--------changed:" + arg0);
+            }
 
-             @Override
-             public void onPageScrolled(int arg0, float arg1, int arg2) {
-                 Log.d(TAG, "-------scrolled arg0:" + arg0);
-                 Log.d(TAG, "-------scrolled arg1:" + arg1);
-                 Log.d(TAG, "-------scrolled arg2:" + arg2);
-             }
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+                Log.d(TAG, "-------scrolled arg0:" + arg0);
+                Log.d(TAG, "-------scrolled arg1:" + arg1);
+                Log.d(TAG, "-------scrolled arg2:" + arg2);
+            }
 
-             @Override
-             public void onPageSelected(int arg0) {
-                 Log.d(TAG, "------selected:" + arg0);
-             }
-         });
+            @Override
+            public void onPageSelected(int arg0) {
+                Log.d(TAG, "------selected:" + arg0);
+            }
+        });
 
          /*---------------------------------- pager content  ----------------------------------------------*/
-         ListView listView = (ListView) view1.findViewById(R.id.lv_recipe);
-         RecipeAdapter adapter= new RecipeAdapter(this);
-         listView.setAdapter(adapter);
-         listView.setOnItemClickListener(new OnItemClickListener() {
-             @Override
-             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                 Intent intent = new Intent(MainActivity.this, RecipeDetailActivity.class);
-                 startActivity(intent);
-             }
-         });
+        ListView listView = (ListView) view1.findViewById(R.id.lv_recipe);
+        RecipeAdapter adapter = new RecipeAdapter(this);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, RecipeDetailActivity.class);
+                startActivity(intent);
+            }
+        });
 
-         ( (Button) view3.findViewById(R.id.bt_plogin)).setOnClickListener( new OnClickListener(){
-             @Override
-             public void onClick(View v) {
-                 Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-                 startActivity(intent);
-             }
-         });
-     }
+        ((Button) view3.findViewById(R.id.bt_plogin)).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 
 
- }
+}
