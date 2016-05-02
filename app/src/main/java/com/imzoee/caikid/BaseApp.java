6fa05c739a47c8c94608ace632d5d285a6d33f81
zoey <zoey1752@163.com>
2016-05-03 03:47:05 +0800

    auto login, but not perfect, need improve

--- app/src/main/java/com/imzoee/caikid/BaseApp.java
+++ app/src/main/java/com/imzoee/caikid/BaseApp.java
@@ -2,12 +2,26 @@ package com.imzoee.caikid;
 
 import android.app.Application;
 import android.content.res.Configuration;
+import android.util.Log;
+import android.widget.Toast;
 
+import com.imzoee.caikid.convention.ConstConv;
 import com.imzoee.caikid.dao.User;
+import com.imzoee.caikid.fragment.MeFragment;
+import com.imzoee.caikid.fragment.OrderFragment;
 import com.imzoee.caikid.utils.api.HttpClient;
+import com.imzoee.caikid.utils.api.UserApiInterface;
+import com.imzoee.caikid.utils.misc.ObservablesFactory;
 import com.imzoee.caikid.utils.preferences.UserPref;
 import com.imzoee.caikid.utils.preferences.Settings;
-import com.imzoee.caikid.utils.api.CaikidCookieJar;
+
+import okhttp3.Headers;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import rx.Observable;
+import rx.Subscriber;
 
 /**
  * Created by zoey on 2016/4/21.
@@ -36,29 +50,21 @@ public class BaseApp extends Application {
         settings = new Settings(this);
         httpClient = new HttpClient();
 
+        Log.i("--------auto-login----",String.valueOf(settings.isAutoLogin()));
 
-/*
-        if( Settings.isAutoLogin(this) ){
-            int id = UserPref.getPfUserId(this);
-            String account = UserPref.getPfUserAccount(this);
-            String pwd = UserPref.getPfUserPwd(this);
+        if( settings.isAutoLogin() && settings.isLogin() ){
+            Log.i("------------ss---","auto");
+            String account = userPref.getPfUserAccount();
+            String pwd = userPref.getPfUserPwd();
 
-            int status = HttpClient.login(account, pwd);
-            switch (status){
-                case LoginConv.LOGIN_SUCCESSED :
-                    setLogin(true);
-                    setSessionUser(HttpClient.getUser(id, account, "zoey", pwd)
-                                 );
-                    break;
-                case LoginConv.LOGIN_PWD_INCORRECT:
-                    setLogin(false);
-                    break;
-                case LoginConv.LOGIN_USER_NOT_EXIT:
-                    setLogin(false);
-                    break;
-            }
-        }*/
+            UserApiInterface i = httpClient.getUserApiInterface();
+            Call<User> loginCall = i.login(account, pwd);
+
+            loginCall.enqueue(new LoginCallBack());
+        }
     }
+
+
 
     @Override
     public void onLowMemory() {
@@ -86,4 +92,54 @@ public class BaseApp extends Application {
     public static UserPref getUserPref(){ return instance.userPref; }
 
     public static HttpClient getHttpClient() { return instance.httpClient; }
+
+
+    private class LoginCallBack implements Callback<User> {
+        @Override
+        public void onResponse(Call<User> call, Response<User> response){
+
+            Headers headers = response.headers();
+            String status = headers.get(ConstConv.HEADKEY_RESPONSTATUS);
+
+            Log.i("---------b-----------", response.headers().toString());
+
+            if(status != null && status.equals(ConstConv.RET_STATUS_OK)){
+
+                final User user = response.body();
+
+                /* save the basic information to userPref and settings */
+                BaseApp.getUserPref().setPfUser(user);
+                BaseApp.getSettings().setLoginStatus(true);
+
+                /* alert the relative components to update their data or view */
+                Observable<User> logoutObservable = ObservablesFactory.loginStateObservable(user);
+                Subscriber<User> orderLogoutSubscriber = OrderFragment.getLoginStateSubscriber();
+                Subscriber<User> meLogoutSubscriber = MeFragment.getLoginStateSubscriber();
+                if (orderLogoutSubscriber != null){
+                    logoutObservable.subscribe(orderLogoutSubscriber);
+                }
+                if (meLogoutSubscriber != null) {
+                    logoutObservable.subscribe(meLogoutSubscriber);
+                }
+
+            } else {
+                settings.setLoginStatus(false);
+            }
+        }
+
+        @Override
+        public void onFailure(Call<User> call, Throwable t){
+
+            settings.setLoginStatus(false);
+
+            if(t instanceof java.net.ConnectException){
+                CharSequence msg = getString(R.string.msg_connect_error);
+                Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getBaseContext(), t.getMessage(), Toast.LENGTH_LONG).show();
+                Log.i("+++++++++++++++++++++++", t.toString());
+                t.printStackTrace();
+            }
+        }
+    }
 }
