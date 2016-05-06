460a9b8b3cac0ae205cbb41afb6d42432420fdf0
zoey <zoey1752@163.com>
2016-05-06 16:29:25 +0800

    handle unknown return status.

--- app/src/main/java/com/imzoee/caikid/activity/LoginActivity.java
+++ app/src/main/java/com/imzoee/caikid/activity/LoginActivity.java
@@ -359,6 +359,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 Toast.makeText(getBaseContext(),
                         getString(R.string.msg_time_out),
                         Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(getBaseContext(),
+                        getString(R.string.msg_unknown_ret_status),
+                        Toast.LENGTH_LONG).show();
             }
             showProgress(false);
         }
