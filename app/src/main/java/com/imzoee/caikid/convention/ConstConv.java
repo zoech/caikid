5d36c6e92184e996f62ace250bd0f17fe59d8ade
zoey <zoey1752@163.com>
2016-05-08 22:01:34 +0800

    change server ip to the new test server ip

--- app/src/main/java/com/imzoee/caikid/convention/ConstConv.java
+++ app/src/main/java/com/imzoee/caikid/convention/ConstConv.java
@@ -7,7 +7,8 @@ package com.imzoee.caikid.convention;
  */
 public class ConstConv {
     /* the server ip */
-    public static final String API_URL = "http://110.64.86.208:8080/caikid/";
+    //public static final String API_URL = "http://110.64.86.208:8080/caikid/";
+	public static final String API_URL = "http://192.168.1.119/app/";
 
     //public static final String HEADKEY_SESSIONID = "Caikid-SessionID";
     public static final String HEADKEY_RESPONSTATUS = "Caikid-ResponseStatus";
