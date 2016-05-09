b8f125b628a9490b9cb5e1f0dd9abeaec26df6ff
zoey <zoey1752@163.com>
2016-05-09 23:40:07 +0800

    basic data display

--- app/src/main/java/com/imzoee/caikid/activity/CartActivity.java
+++ app/src/main/java/com/imzoee/caikid/activity/CartActivity.java
@@ -5,10 +5,16 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.imzoee.caikid.BaseApp;
 import com.imzoee.caikid.R;
+import com.imzoee.caikid.adapter.CartItemAdapter;
 import com.imzoee.caikid.model.CaikidCart;
+import com.rey.material.widget.ListView;
 
 import rx.Subscriber;
 
@@ -20,6 +26,10 @@ import rx.Subscriber;
 public class CartActivity extends AppCompatActivity {
     private static CartActivity instance = null;
 
+    private CaikidCart cart = null;
+    private CartItemAdapter cartItemAdapter = null;
+    private ListView lvContent = null;
+    private TextView tvTotalPrice = null;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -27,21 +37,30 @@ public class CartActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cart);
         instance = this;
 
+        cart = BaseApp.getCart();
         initView();
         initData();
         initListener();
     }
 
     private void initView(){
-
+        lvContent = (ListView) findViewById(R.id.lv_content);
+        tvTotalPrice = (TextView) findViewById(R.id.tv_total_price);
     }
 
     private void initData(){
-
+        tvTotalPrice.setText(String.valueOf(cart.getTotallPrice()));
+        cartItemAdapter = new CartItemAdapter(getBaseContext());
+        lvContent.setAdapter(cartItemAdapter);
     }
 
     private void initListener(){
+        lvContent.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
+            }
+        });
     }
 
     private Subscriber<CaikidCart> createCartActionSubscriber(){
@@ -80,6 +99,9 @@ public class CartActivity extends AppCompatActivity {
             } else {
 
             }
+
+            tvTotalPrice.setText(String.valueOf(cart.getTotallPrice()));
+            cartItemAdapter.notifyDataSetChanged();
         }
     }
 }
