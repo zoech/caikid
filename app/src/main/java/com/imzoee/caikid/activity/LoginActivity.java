df3fcd181b55d7add2d1df1d92ab0c61706c89c9
zoey <zoey1752@163.com>
2016-05-15 01:29:50 +0800

    fix a little bug

--- app/src/main/java/com/imzoee/caikid/activity/LoginActivity.java
+++ app/src/main/java/com/imzoee/caikid/activity/LoginActivity.java
@@ -187,7 +187,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) || !TextCheck.validPwd(password)) {
+        if (TextUtils.isEmpty(password) || !TextCheck.validPwd(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
