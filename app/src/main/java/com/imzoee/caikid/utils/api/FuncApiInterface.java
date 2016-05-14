496de9d3e46c70b6f3e7a5689a6d65e28c7cf597
zoey <zoey1752@163.com>
2016-05-15 00:17:03 +0800

    order interface testable

--- app/src/main/java/com/imzoee/caikid/utils/api/FuncApiInterface.java
+++ app/src/main/java/com/imzoee/caikid/utils/api/FuncApiInterface.java
@@ -2,8 +2,6 @@ package com.imzoee.caikid.utils.api;
 
 import com.imzoee.caikid.convention.ConstConv;
 
-import java.util.Date;
-
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
@@ -18,10 +16,10 @@ import retrofit2.http.POST;
  */
 public interface FuncApiInterface {
     @FormUrlEncoded
-    @POST("makeOrder")
+    @POST("makeOrder.php")
     Call<ResponseBody> order(@Field(ConstConv.RESKEY_ORDER_CART) String cartContent,
                              @Field(ConstConv.RESKEY_ORDER_ADDR) String addr,
                              @Field(ConstConv.RESKEY_ORDER_PHONE) String phone,
                              @Field(ConstConv.RESKEY_ORDER_NAME) String name,
-                             @Field(ConstConv.RESKEY_ORDER_RECIEVETIME) Date date);
+                             @Field(ConstConv.RESKEY_ORDER_RECIEVETIME) String date);
 }
