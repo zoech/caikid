4877d25f57fd8618bd847ace6eedfb823144d723
zoey <zoey1752@163.com>
2016-05-15 00:07:56 +0800

    update

--- app/src/main/java/com/imzoee/caikid/utils/misc/TextCheck.java
+++ app/src/main/java/com/imzoee/caikid/utils/misc/TextCheck.java
@@ -1,24 +1,56 @@
 package com.imzoee.caikid.utils.misc;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * Created by zoey on 2016/4/29.
  *
  * Interface related to text checking.
  */
 public class TextCheck {
-    public static boolean validAccount(String account){
-        if(account.contains(" ")){
-            return false;
-        }
-        return true;
+    /**
+     * Determine whether a string is a valid password.
+     *
+     * @param str
+     * The string that you want to judge if it's a valid password.
+     *
+     * @return
+     * True if str is a valid password, otherwise false.
+     */
+    public static boolean validPwd(String str){
+        Pattern pattern = Pattern.compile("^[a-zA-Z]\\w{5,17}$");
+        Matcher m = pattern.matcher(str);
+        return m.matches();
     }
 
-    public static boolean validPwd(String pwd){
-        for(int i = 0; i < pwd.length(); ++i){
-            if(pwd.charAt(i) < 33 || pwd.charAt(i) > 126){
-                return false;
-            }
-        }
-        return true;
+    /**
+     * Determine whether a string is a valid handset number.
+     *
+     * @param str
+     * The string that you want to judge if it's a valid handset number.
+     *
+     * @return
+     * True if str is a valid handset number, otherwise false.
+     */
+    public static boolean validHandset(String str){
+        Pattern pattern = Pattern.compile("^((13[0-9])|(15[^4,\\D])|(18[0-9]))\\d{8}$");
+        Matcher m = pattern.matcher(str);
+        return m.matches();
+    }
+
+    /**
+     * Determine whether a string is a valid email address.
+     *
+     * @param str
+     * The string that you want to judge if it's a valid email address.
+     *
+     * @return
+     * True if str is a valid email address, otherwise false.
+     */
+    public static boolean validEmail(String str){
+        Pattern pattern = Pattern.compile("^([a-z0-9A-Z]+[-|_|\\.]?)+[a-z0-9A-Z]@([a-z0-9A-Z]+(-[a-z0-9A-Z]+)?\\.)+[a-zA-Z]{2,}$");
+        Matcher m = pattern.matcher(str);
+        return m.matches();
     }
 }
