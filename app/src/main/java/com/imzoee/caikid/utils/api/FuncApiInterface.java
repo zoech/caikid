37c6ef0ce7b8ee164f0b3eb9e2fc880df44e76af
zoey <zoey1752@163.com>
2016-06-04 00:47:12 +0800

    add an interface to get a list of order of a user

--- app/src/main/java/com/imzoee/caikid/utils/api/FuncApiInterface.java
+++ app/src/main/java/com/imzoee/caikid/utils/api/FuncApiInterface.java
@@ -1,12 +1,17 @@
 package com.imzoee.caikid.utils.api;
 
 import com.imzoee.caikid.convention.ConstConv;
+import com.imzoee.caikid.dao.Order;
+
+import java.util.List;
 
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 /**
  * Created by zoey on 2016/5/13.
@@ -22,4 +27,7 @@ public interface FuncApiInterface {
                              @Field(ConstConv.RESKEY_ORDER_PHONE) String phone,
                              @Field(ConstConv.RESKEY_ORDER_NAME) String name,
                              @Field(ConstConv.RESKEY_ORDER_RECIEVETIME) String date);
+
+    @GET("getOrderList.php")
+    Call<List<Order>> getOrderList(@Query("orderBy") String orderType);
 }
